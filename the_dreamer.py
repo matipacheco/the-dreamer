db5a79391ea95c1858d1e9658dc45b3475fea29a
Mati <matipacheco@gmail.com>
2018-08-21 20:41:29 -0300

    Could not live with that text replication

--- the_dreamer.py
+++ the_dreamer.py
@@ -4,7 +4,8 @@ from prettytable import PrettyTable
 
 class TheDreamer:
 	def __init__(self):
-		site      = urlopen("https://careers.blizzard.com/es-mx/openings/engineering,information-technology/all/all/all/1")
+		self.url  = "https://careers.blizzard.com"
+		site      = urlopen(self.url + "/es-mx/openings/engineering,information-technology/all/all/all/1")
 		self.soup = BeautifulSoup(site, "html.parser")
 
 	def look_for_jobs(self):
@@ -13,7 +14,7 @@ class TheDreamer:
 		table = PrettyTable(["URL", "Position", "Project", "Location", "Position type"])
 
 		for job in job_oppening:
-			link          = job.get('href')
+			link          = self.url + job.get('href')
 			position      = job.contents[0].get_text()
 			area          = job.contents[1].get_text()
 			location      = job.contents[2].get_text()
